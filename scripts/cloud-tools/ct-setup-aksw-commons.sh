a3d4d2579f1f503acb4702b97e964e4b95d999fb
aklakan <aklakan@localhost>
2011-05-27 11:27:08 +0200

    Script for setting up aksw commons cloud tools. Copy this script to the machine where you want to set it up.

--- scripts/cloud-tools/ct-setup-aksw-commons.sh
+++ scripts/cloud-tools/ct-setup-aksw-commons.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #Checks out aksw-commons under opt as the current user and group
 
+# Install mercurial (if not installed already)
+sudo apt-get -y install mercurial
+
 # TODO Check if directory already exists and is non empty
 # In that case only update the repo and recreate the symlinks
 
 dir="/opt/aksw-commons/"
 sudo mkdir -p $dir
-chown $USER:$USER $dir
+sudo chown $USER:$USER $dir
 
 hg clone https://aksw-commons.googlecode.com/hg/ /opt/aksw-commons
 
