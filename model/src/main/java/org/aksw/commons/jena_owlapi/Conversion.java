2264b350867b03e67b549c621b5db52b0af2a1be
RavenArkadon <RavenArkadon@localhost>
2011-09-15 11:16:06 +0200

    Conversion for jena - owl api

--- model/src/main/java/org/aksw/commons/jena_owlapi/Conversion.java
+++ model/src/main/java/org/aksw/commons/jena_owlapi/Conversion.java
@@ -4,15 +4,27 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import org.aksw.commons.jena.Constants;
 import org.aksw.commons.jena.StringConverter;
 import org.apache.log4j.Logger;
+import org.coode.owlapi.rdf.model.RDFGraph;
+import org.coode.owlapi.rdf.model.RDFLiteralNode;
+import org.coode.owlapi.rdf.model.RDFNode;
+import org.coode.owlapi.rdf.model.RDFTriple;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.util.Collections;
+import java.util.Set;
 
 /**
  * Converts between Jena and OWLApi models
@@ -60,5 +72,86 @@ public class Conversion {
 
     }
 
+
+    public static Set<RDFTriple> extractTriples(RDFGraph graph)
+    {
+        Field field = null;
+        try {
+            field = graph.getClass().getDeclaredField("triples");
+
+            boolean saved = field.isAccessible();
+            field.setAccessible(true);
+
+            Set<RDFTriple> triples = (Set<RDFTriple>)field.get(graph);
+            field.setAccessible(saved);
+
+            return triples;
+        } catch (Exception e) {
+            throw new RuntimeException("Should not happen", e);
+        }
+    }
+
+
+    public static String toStringNTriples(RDFGraph graph) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        writeNTriples(graph, out);
+
+        return out.toString();
+    }
+
+    public static void writeNTriples(RDFGraph graph, OutputStream out)
+    {
+        writeNTriples(graph, new PrintStream(out));
+    }
+
+    public static void writeNTriples(RDFGraph graph, PrintStream out)
+    {
+        Set<RDFTriple> triples = extractTriples(graph);
+
+        for(RDFTriple triple : triples) {
+           out.println(toStringNTriples(triple) + "\n");
+        }
+    }
+
+    public static String toStringNTriples(RDFTriple triple)
+    {
+        return toStringSparql(triple) + " .";
+    }
+
+    public static String toStringSparql(RDFTriple triple)
+	{
+		return
+			toString(triple.getSubject()) + " " +
+			toString(triple.getProperty()) + " " +
+			toString(triple.getObject());
+	}
+
+	public static String toString(RDFNode node)
+	{
+		if(node.isAnonymous())
+			return "_:a" + node.toString();
+		else if(node.isLiteral()) {
+			RDFLiteralNode n = (RDFLiteralNode)node;
+
+			String literal = sparqlEscapeLiteral(n.getLiteral());
+			String result = "\"\"\"" + literal + "\"\"\"";
+
+			if(n.getLang() != null && !n.getLang().isEmpty())
+				result += "@" + n.getLang().toLowerCase();
+
+			if(n.getDatatype() != null)
+				result += "^^" + "<" + n.getDatatype() + ">";
+
+			return result;
+		}
+		else // resource
+			return  node.toString();
+	}
+
+    public static String sparqlEscapeLiteral(String literal)
+    {
+        return literal.replace("\"", "\\\"");
+    }
 
 }
