03d9ef8aa3d80664bd85361cced0612cb27bb947
RavenArkadon <RavenArkadon@localhost>
2012-12-28 17:16:44 +0100

    Added a simple MapReader (for TSV files) utility class

---
+++ util/src/main/java/org/aksw/commons/util/MapReader.java
@@ -0,0 +1,62 @@
+package org.aksw.commons.util;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A simple utility class for reading Map<String, String> objects from
+ * two column TSV files. Lines whose first non-whitespace character is '#' are
+ * treated as comments.
+ * 
+ * @author Claus Stadler <cstadler@informatik.uni-leipzig.de>
+ *
+ */
+public class MapReader {
+	private static final Logger logger = LoggerFactory
+			.getLogger(MapReader.class);
+
+	public static Map<String, String> readFile(File file) throws IOException {
+
+		Map<String, String> result = new HashMap<String, String>();
+
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		try {
+		
+			String line = null;
+			while((line = reader.readLine()) != null) {
+				
+				line = line.trim();
+				if(line.isEmpty() || line.startsWith("#")) {
+					continue;
+				}
+				
+				String[] parts = line.split("\\s+", 2);
+				if(parts.length != 2) {
+					logger.debug("No mapping for: " + parts[0]);
+					continue;
+				}
+				
+				
+				result.put(parts[0], parts[1]);				
+			}
+		} finally {
+			reader.close();
+		}
+	
+		return result;
+		
+	}
+	/*
+	 * public Map<String, String> read(File file) throws IOException { }
+	 */
+}
