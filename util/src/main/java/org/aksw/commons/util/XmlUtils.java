955f2aa46cc836e18e5b3e783e7a73041b497bf7
RavenArkadon <RavenArkadon@localhost>
2013-09-15 03:02:35 +0200

    Added util function to unmarshal xml

--- util/src/main/java/org/aksw/commons/util/XmlUtils.java
+++ util/src/main/java/org/aksw/commons/util/XmlUtils.java
@@ -1,19 +1,37 @@
 package org.aksw.commons.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
 
 
 public class XmlUtils
@@ -135,4 +153,24 @@ public class XmlUtils
 		return strModificationDate;
 	}
 	
+    
+	@SuppressWarnings("unchecked")
+	public static <T> T unmarshallXml(Class<T> clazz, InputStream in)
+			throws JAXBException, UnsupportedEncodingException {
+		
+		String className = clazz.getPackage().getName();
+		JAXBContext context = JAXBContext.newInstance(className);	
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+
+		//Reader reader = new IgnoreIllegalCharactersXmlReader(in);
+	
+		Reader reader = new InputStreamReader(in,"UTF-8");
+		 
+		InputSource is = new InputSource(reader);
+		is.setEncoding("UTF-8");
+		
+		Object result = unmarshaller.unmarshal(is); //file);
+		return (T)result;
+	}
+
 }
