d1c5f97f9d3a2a608368c3e3be845ae07f8104b2
Aklakan <Aklakan@localhost>
2014-10-06 13:55:09 +0200

    Removed old names....

--- util/src/main/java/org/aksw/commons/util/factory/Factory2.java
+++
@@ -1,5 +0,0 @@
-package org.aksw.commons.util.factory;
-
-public interface Factory2<T> {
-	T create(T a, T b);
-}
--- util/src/main/java/org/aksw/commons/util/factory/FactoryN.java
+++
@@ -1,7 +0,0 @@
-package org.aksw.commons.util.factory;
-
-import java.util.List;
-
-public interface FactoryN<T> {
-	T create(List<T> args);
-}
