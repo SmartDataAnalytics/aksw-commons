2b385869ba7b07a36b1852e255d458434e2e7e72
Claus Stadler <RavenArkadon@googlemail.com>
2016-09-22 19:10:03 +0200

    Added unit test

--- aksw-commons-collections/src/test/java/org/aksw/commons/collections/CartesianIteratorTest.java
+++ aksw-commons-collections/src/test/java/org/aksw/commons/collections/CartesianIteratorTest.java
@@ -1,9 +1,17 @@
 package org.aksw.commons.collections;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.Lists;
 
 /**
  * @author Claus Stadler
@@ -12,6 +20,39 @@ import java.util.List;
  * Time: 11:01 PM
  */
 public class CartesianIteratorTest {
+
+	public static void main(String[] args) {
+		Stopwatch sw = Stopwatch.createStarted();
+		for(int y = 0; y < 10; ++y) {
+			new CartesianIteratorTest().testCartesianProduct();
+
+			//System.out.println("done[" + y + "]: " + i);
+		}
+		System.out.println(sw.elapsed(TimeUnit.MILLISECONDS));
+	}
+
+	@Test
+	public void testCartesianProduct() {
+		List<Integer> ints = IntStream.range(0, 10).mapToObj(x -> x).collect(Collectors.toList());
+
+		List<List<Integer>> arr = Arrays.asList(ints, ints, ints);
+		Iterator<List<Integer>> itA = Lists.cartesianProduct(arr).iterator();
+		Iterator<List<Integer>> itB = CartesianProduct.create(arr).iterator();
+
+		while(itA.hasNext() && itB.hasNext()) {
+			List<Integer> a = itA.next();
+			List<Integer> b = itB.next();
+
+			if(!a.equals(b)) {
+				Assert.assertEquals(a, b);
+			}
+		}
+
+		boolean unfinished = itA.hasNext() || itB.hasNext();
+		Assert.assertFalse(unfinished);
+	}
+
+
     // The implementation of the StackCartesianProductIterator does not terminate
     //@Test
     public void testStack() {
