b79eec0d673090817bc7a5bdf52e9a2dc27039b6
Claus Stadler <RavenArkadon@googlemail.com>
2017-02-07 01:08:03 +0100

    added another ctor to ReversibleMapImpl

--- aksw-commons-collections/src/main/java/org/aksw/commons/collections/reversible/ReversibleMapImpl.java
+++ aksw-commons-collections/src/main/java/org/aksw/commons/collections/reversible/ReversibleMapImpl.java
@@ -10,67 +10,72 @@ import com.google.common.collect.SetMultimap;
 
 
 public class ReversibleMapImpl<K, V>
-	extends AbstractMap<K, V>
-	implements ReversibleMap<K, V>
+    extends AbstractMap<K, V>
+    implements ReversibleMap<K, V>
 {
-	protected ReversibleSetMultimap<V, K> reverse;
+    protected ReversibleSetMultimap<V, K> reverse;
 
-	protected Map<K, V> forward;
-	protected SetMultimap<V, K> backward;
+    protected Map<K, V> forward;
+    protected SetMultimap<V, K> backward;
 
-	public ReversibleMapImpl() {
-		this.forward = new HashMap<>();
-		this.backward = HashMultimap.create();
+    public ReversibleMapImpl() {
+        this(new HashMap<>(), HashMultimap.create());
+    }
 
-		this.reverse = new ReversibleSetMultimapImpl<>(this, this.backward);
-	}
+   public ReversibleMapImpl(Map<K, V> forward, SetMultimap<V, K> backward) {
+        this.forward = forward;
+        this.backward = backward;
 
-	@Override
-	public V put(K key, V value) {
-		V result = forward.get(key);
-		backward.remove(result, key);
+        this.reverse = new ReversibleSetMultimapImpl<>(this, this.backward);
+    }
 
-		forward.put(key, value);
-		backward.put(value, key);
 
-		return result;
-	}
+    @Override
+    public V put(K key, V value) {
+        V result = forward.get(key);
+        backward.remove(result, key);
 
-	@Override
-	public V remove(Object key) {
-		V result = forward.remove(key);
-		backward.remove(result, key);
-		return result;
-	}
+        forward.put(key, value);
+        backward.put(value, key);
 
-	@Override
-	public void clear() {
-		forward.clear();
-		backward.clear();
-	}
+        return result;
+    }
 
-	@Override
-	public V get(Object key) {
-		return forward.get(key);
-	}
+    @Override
+    public V remove(Object key) {
+        V result = forward.remove(key);
+        backward.remove(result, key);
+        return result;
+    }
 
-	@Override
-	public boolean containsKey(Object key) {
-		return forward.containsKey(key);
-	}
+    @Override
+    public void clear() {
+        forward.clear();
+        backward.clear();
+    }
 
-	@Override
-	public boolean containsValue(Object value) {
-		return backward.containsKey(value);
-	}
+    @Override
+    public V get(Object key) {
+        return forward.get(key);
+    }
 
-	@Override
-	public ReversibleSetMultimap<V, K> reverse() {
-		return reverse;
-	}
+    @Override
+    public boolean containsKey(Object key) {
+        return forward.containsKey(key);
+    }
 
-	@Override
-	public Set<Entry<K, V>> entrySet() {
-		return forward.entrySet();
-	}
+    @Override
+    public boolean containsValue(Object value) {
+        return backward.containsKey(value);
+    }
+
+    @Override
+    public ReversibleSetMultimap<V, K> reverse() {
+        return reverse;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return forward.entrySet();
+    }
 }
