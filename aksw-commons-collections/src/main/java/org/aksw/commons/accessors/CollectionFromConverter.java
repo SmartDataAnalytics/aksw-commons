6213d8055e9cc8a87421107b19d1dbf647172085
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2020-07-07 18:57:01 +0200

    Added filter collection view with remove support (which is not part of guava)

--- aksw-commons-collections/src/main/java/org/aksw/commons/accessors/CollectionFromConverter.java
+++ aksw-commons-collections/src/main/java/org/aksw/commons/accessors/CollectionFromConverter.java
@@ -4,118 +4,116 @@ import java.util.AbstractCollection;
 import java.util.Collection;
 import java.util.Iterator;
 
-import org.aksw.commons.collections.SinglePrefetchIterator;
-
 import com.google.common.base.Converter;
 
 public class CollectionFromConverter<F, B, C extends Collection<B>>
-	extends AbstractCollection<F>
+    extends AbstractCollection<F>
 {
-	public static class IteratorFromConverter<T, U, I extends Iterator<U>>
-		implements Iterator<T>
-	{
-		protected I core;
-		protected Converter<U, T> converter;
+    public static class IteratorFromConverter<T, U, I extends Iterator<U>>
+        implements Iterator<T>
+    {
+        protected I core;
+        protected Converter<U, T> converter;
 
-		public IteratorFromConverter(I core, Converter<U, T> converter) {
-			super();
-			this.core = core;
-			this.converter = converter;
-		}
+        public IteratorFromConverter(I core, Converter<U, T> converter) {
+            super();
+            this.core = core;
+            this.converter = converter;
+        }
 
-		@Override
-		public T next() {
-			U raw = core.next();
-			T result = converter.convert(raw);
-			return result;
-		}
+        @Override
+        public T next() {
+            U raw = core.next();
+            T result = converter.convert(raw);
+            return result;
+        }
 
-		@Override
-		public boolean hasNext() {
-			boolean result = core.hasNext();
-			return result;
-		}
-		
-		@Override
-		public void remove() {
-			core.remove();
-		}
-	}
-	
-	
-	protected C backend;
-	protected Converter<B, F> converter;
-	
-	public CollectionFromConverter(C backend, Converter<B, F> converter) {
+        @Override
+        public boolean hasNext() {
+            boolean result = core.hasNext();
+            return result;
+        }
+
+        @Override
+        public void remove() {
+            core.remove();
+        }
+    }
+
+    protected C backend;
+    protected Converter<B, F> converter;
+
+    public CollectionFromConverter(C backend, Converter<B, F> converter) {
 //		Objects.requireNonNull(backend);
 //		Objects.requireNonNull(converter);
-		
-		this.backend = backend;
-		this.converter = converter;
-	}
 
-	@Override
-	public boolean add(F value) {
-		B item = converter.reverse().convert(value);
-		boolean result = backend.add(item);
-		
-		return result;
-	}
-	
-	@Override
-	public boolean contains(Object o) {
-		boolean result;
-		try {
-			B item = converter.reverse().convert((F)o);
-			result = backend.contains(item);
-		} catch(ClassCastException e) {
-			result = false;
-		}
-		
-		return result;
+        this.backend = backend;
+        this.converter = converter;
+    }
 
-	}
-	
-	@Override
-	public boolean remove(Object o) {
-		boolean result;
-		try {
-			B item = converter.reverse().convert((F)o);
-			result = backend.remove(item);
-		} catch(ClassCastException e) {
-			result = false;
-		}
-		
-		return result;
-	}
+    @Override
+    public boolean add(F value) {
+        B item = converter.reverse().convert(value);
+        boolean result = backend.add(item);
 
-	@Override
-	public Iterator<F> iterator() {
-		Iterator<B> baseIt = backend.iterator();
+        return result;
+    }
 
-		return new SinglePrefetchIterator<F>() {
-			@Override
-			protected F prefetch() throws Exception {
-				while(baseIt.hasNext()) {
-					B b = baseIt.next();
-					F f;
-					try {
-						f = converter.convert(b);
-					} catch(Exception e) {
-						/* Ignore items that fail to convert */
-						continue;
-					}
-					return f;
-				}
-				return finish();
-			}
-			@Override
-			public void doRemove(F item) { baseIt.remove(); }
-		};
-	}
-	
-	@Override
-	public int size() {
-		return backend.size();
-	}
+    @Override
+    public boolean contains(Object o) {
+        boolean result;
+        try {
+            B item = converter.reverse().convert((F)o);
+            result = backend.contains(item);
+        } catch(ClassCastException e) {
+            result = false;
+        }
+
+        return result;
+
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        boolean result;
+        try {
+            B item = converter.reverse().convert((F)o);
+            result = backend.remove(item);
+        } catch(ClassCastException e) {
+            result = false;
+        }
+
+        return result;
+    }
+
+    @Override
+    public Iterator<F> iterator() {
+        Iterator<B> baseIt = backend.iterator();
+
+        Iterator<F> result = new IteratorFromConverter<>(baseIt, converter);
+        return result;
+    }
+
+    @Override
+    public int size() {
+        return backend.size();
+    }
+
+    /**
+     * Wraps the backend with a guava Collection2.filteredCollection that filters out
+     * items for whith the converter raises an exception
+     *
+     * @param <F>
+     * @param <B>
+     * @param <C>
+     * @param backend
+     * @param converter
+     * @return
+     */
+    public static <F, B, C extends Collection<B>> Collection<F> createSafe(Collection<B> backend, Converter<B, F> converter) {
+        Collection<B> safeBackend = ConverterUtils.safeCollection(backend, converter);
+        Collection<F> result = new CollectionFromConverter<>(safeBackend, converter);
+        return result;
+    }
 }
+
