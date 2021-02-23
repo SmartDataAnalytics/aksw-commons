8b098769b1f67f0bba2d8b90e7a9717251fbee15
Claus Stadler <RavenArkadon@googlemail.com>
2021-02-23 22:03:07 +0100

    Formatting

--- aksw-commons-entity-codecs-parent/aksw-commons-entity-codecs-sql/src/main/java/org/aksw/commons/sql/codec/api/SqlCodec.java
+++ aksw-commons-entity-codecs-parent/aksw-commons-entity-codecs-sql/src/main/java/org/aksw/commons/sql/codec/api/SqlCodec.java
@@ -9,31 +9,31 @@ import org.aksw.commons.codec.entity.api.EntityCodec;
  *
  */
 public interface SqlCodec {
-	
+
 	/** Codec for aliases - must never return null */
 	EntityCodec<String> forAlias();
 
-    /** Codec for table names - must never return null */
+	/** Codec for table names - must never return null */
 	EntityCodec<String> forTableName();
 
-    /** Codec for schema names - must never return null */
+	/** Codec for schema names - must never return null */
 	EntityCodec<String> forSchemaName();
 
-    /** Codec for column names - must never return null */
+	/** Codec for column names - must never return null */
 	EntityCodec<String> forColumnName();
-    
-    /**
-     * Codec for string literals - must never return null.
-     * 
-     * Note that serializing non-string types may still use string escaping as part
-     * of the serialization process. E.g. a Date may be serialized as;
-     * 
-     * DATE 'str'
-     * 
-     * http://dev.mysql.com/doc/refman/5.7/en/date-and-time-literals.html
-     * 
-     * @param str
-     * @return
-     */
+
+	/**
+	 * Codec for string literals - must never return null.
+	 * 
+	 * Note that serializing non-string types may still use string escaping as part
+	 * of the serialization process. E.g. a Date may be serialized as;
+	 * 
+	 * DATE 'str'
+	 * 
+	 * http://dev.mysql.com/doc/refman/5.7/en/date-and-time-literals.html
+	 * 
+	 * @param str
+	 * @return
+	 */
 	EntityCodec<String> forStringLiteral();
 }
