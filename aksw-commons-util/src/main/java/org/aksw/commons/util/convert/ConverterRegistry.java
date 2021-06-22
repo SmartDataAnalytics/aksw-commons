7510af0b61b5540981e295732da3a35ee891b27d
Claus Stadler <RavenArkadon@googlemail.com>
2021-06-22 22:08:57 +0200

    Update to the converter registry

--- aksw-commons-util/src/main/java/org/aksw/commons/util/convert/ConverterRegistry.java
+++ aksw-commons-util/src/main/java/org/aksw/commons/util/convert/ConverterRegistry.java
@@ -4,36 +4,36 @@ import java.lang.reflect.Method;
 import java.util.function.Function;
 
 public interface ConverterRegistry {
-	
-	Converter getConverter(Class<?> from, Class<?> to);
-	void register(Converter converter);
-	
-	default <R, J> ConverterRegistry register(
-			Class<R> src,
-			Class<J> tgt,
-			Function<? super R, ? extends J> srcToTgt) {
-		Converter converter = ConverterImpl.create(src, tgt, srcToTgt);
-		register(converter);
-		
-		return this;
-	}
-	
-	default <R, J> ConverterRegistry register(
-			Class<R> src,
-			Class<J> tgt,
-			Function<? super R, ? extends J> srcToTgt,
-			Function<? super J, ? extends R> tgtToSrc) {
-		register(src, tgt, srcToTgt);
-		register(tgt, src, tgtToSrc);
-		
-		return this;
-	}
 
-	default ConverterRegistry register(Method method) {
-		Converter converter = ConverterImpl.create(method);
-		register(converter);
-		
-		return this;
-	}
+    Converter getConverter(Class<?> from, Class<?> to);
+    void register(Converter converter);
+
+    default <R, J> ConverterRegistry register(
+            Class<R> src,
+            Class<J> tgt,
+            Function<? super R, ? extends J> srcToTgt) {
+        Converter converter = ConverterImpl.create(src, tgt, srcToTgt);
+        register(converter);
+
+        return this;
+    }
+
+    default <R, J> ConverterRegistry register(
+            Class<R> src,
+            Class<J> tgt,
+            Function<? super R, ? extends J> srcToTgt,
+            Function<? super J, ? extends R> tgtToSrc) {
+        register(src, tgt, srcToTgt);
+        register(tgt, src, tgtToSrc);
+
+        return this;
+    }
+
+    default ConverterRegistry register(Method method) {
+        Converter converter = ConverterImpl.create(method);
+        register(converter);
+
+        return this;
+    }
 
 }
