d3a116da60c7762ca6f6ee3412c9cfd8f8c45f7c
Claus Stadler <RavenArkadon@googlemail.com>
2021-08-11 13:17:00 +0200

    Added SetSupplier for TreeSet

--- aksw-commons-index/src/main/java/org/aksw/commons/index/util/SetSupplier.java
+++ aksw-commons-index/src/main/java/org/aksw/commons/index/util/SetSupplier.java
@@ -17,50 +17,71 @@
 
 package org.aksw.commons.index.util;
 
+import java.util.Comparator;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.Supplier;
 
 /**
- * Helper interface for creating new set instances with automatically inferred types
+ * Helper interface for creating new set instances with automatically inferred
+ * types
  *
  * @author Claus Stadler 11/09/2020
  *
  */
 public interface SetSupplier {
-  <V> Set<V> get();
+    <V> Set<V> get();
 
-  /**
-   * A supplier that supplies null instead of set instances.
-   * In nested structures such null values may act as placeholders that are replaced in a
-   * post-processing step.
-   *
-   * @return 'null' casted to the appropriate type.
-   */
-  public static SetSupplier none() {
-      return new SetSupplier() {
-        @Override
-        public <V> Set<V> get() {
-            return (Set<V>)null;
+    /**
+     * A supplier that supplies null instead of set instances. In nested structures
+     * such null values may act as placeholders that are replaced in a
+     * post-processing step.
+     *
+     * @return 'null' casted to the appropriate type.
+     */
+    public static SetSupplier none() {
+        return new SetSupplier() {
+            @Override
+            public <V> Set<V> get() {
+                return (Set<V>) null;
+            }
+        };
+    }
+
+    /**
+     * Force cast to the requested type. Useful for e.g. TreeSets: While e.g.
+     * HashSet::new can supply set instances for any generic type, TreeSets are
+     * dependent on a comparator which may only work with specific types.
+     *
+     * @param setSupplier
+     * @return
+     */
+    public static SetSupplier forceCast(Supplier<Set<?>> setSupplier) {
+        return new SetSupplier() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public <V> Set<V> get() {
+                return (Set<V>) setSupplier.get();
+            }
+        };
+    }
+
+    public static SetSupplier forTreeSet(Comparator<?> cmp) {
+        return new SetSupplierTreeSet(cmp);
+    }
+
+    public static class SetSupplierTreeSet implements SetSupplier {
+        protected Comparator<?> cmp;
+
+        public SetSupplierTreeSet(Comparator<?> cmp) {
+            super();
+            this.cmp = cmp;
         }
-    };
-  }
 
-  /**
-   * Force cast to the requested type. Useful for e.g. TreeSets:
-   * While e.g. HashSet::new can supply set instances for any generic type,
-   * TreeSets are dependent on a comparator which may only work with specific types.
-   *
-   * @param setSupplier
-   * @return
-   */
-  public static SetSupplier forceCast(Supplier<Set<?>> setSupplier) {
-      return new SetSupplier() {
-        @SuppressWarnings("unchecked")
+        @SuppressWarnings({ "unchecked", "rawtypes" })
         @Override
-        public <V> Set<V> get() {
-            return (Set<V>)setSupplier.get();
+        public <T> Set<T> get() {
+            return new TreeSet<T>((Comparator) cmp);
         }
-    };
-  }
-
+    }
 }
