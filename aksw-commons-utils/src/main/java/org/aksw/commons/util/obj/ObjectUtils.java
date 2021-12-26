57685a2d3e4ec4cb9769986949c2100546e9f08e
Claus Stadler <RavenArkadon@googlemail.com>
2021-12-27 00:04:22 +0100

    log4j 2.17; moven coalesce to ObjectUtils

--- aksw-commons-utils/src/main/java/org/aksw/commons/util/obj/ObjectUtils.java
+++ aksw-commons-utils/src/main/java/org/aksw/commons/util/obj/ObjectUtils.java
@@ -1,5 +1,8 @@
 package org.aksw.commons.util.obj;
 
+import java.util.Arrays;
+import java.util.function.Supplier;
+
 public class ObjectUtils {
     /** For a given object derive a string of the form className@identityHashCode */
     public static String toStringWithIdentityHashCode(Object obj) {
@@ -9,5 +12,28 @@ public class ObjectUtils {
     /** For a given object derive a string of the form className@identityHashCode */
     public static String toStringWithIdentityHashCode(Object obj, String nullDefault) {
         return obj != null ? obj.getClass().getName() + "@" + System.identityHashCode(obj) : nullDefault;
+    }
+
+    /**
+     * Supplier-based coalesce function as described in
+     * https://benjiweber.co.uk/blog/2013/12/08/null-coalescing-in-java-8/
+     *
+     * <br />
+     * Example usage:
+     * {@code coalesce(obj::getName, obj::getId, () -> obj.hasFoo() ? obj.foo().toString() : "bar") }
+     *
+     *
+     * @param <T>
+     * @param ts
+     * @return
+     */
+    @SafeVarargs
+    public static <T> T coalesce(Supplier<T>... ts) {
+        return Arrays.asList(ts)
+                .stream()
+                .map(t -> t.get())
+                .filter(t -> t != null)
+                .findFirst()
+                .orElse(null);
     }
 }
