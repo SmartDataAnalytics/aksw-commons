d2bd027cbf4809fab562309be38fbfac6cec23c1
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-15 15:58:40 +0100

    Revised converter registry

--- aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/convert/ConverterRegistries.java
+++ aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/convert/ConverterRegistries.java
@@ -3,17 +3,20 @@ package org.aksw.commons.util.convert;
 import com.google.common.primitives.Primitives;
 
 public class ConverterRegistries {
-	/** If the initial lookup fails and any argument is a primitive type then retry with boxed types */
-	public static ConvertFunctionRaw getConverterBoxed(ConverterRegistry registry, Class<?> from, Class<?> to) {
-		ConvertFunctionRaw result = registry.getConverter(from, to);
+    /** If the initial lookup fails and any argument is a primitive type then retry with boxed types */
+    public static ConvertFunctionRaw getConverterBoxed(ConverterRegistry registry, Class<?> from, Class<?> to) {
+        ConvertFunctionRaw result = registry.getConverter(from, to);
 
-		Class<?> wrappedFrom = Primitives.wrap(from);
-		Class<?> wrappedTo = Primitives.wrap(to);
+        // If there was no result then try again with boxed types
+        if (result == null) {
+            Class<?> wrappedFrom = Primitives.wrap(from);
+            Class<?> wrappedTo = Primitives.wrap(to);
 
-		if (wrappedFrom != from || wrappedTo != to) {
-			result = registry.getConverter(wrappedFrom, wrappedTo);
-		}
+            if (wrappedFrom != from || wrappedTo != to) {
+                result = registry.getConverter(wrappedFrom, wrappedTo);
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 }
