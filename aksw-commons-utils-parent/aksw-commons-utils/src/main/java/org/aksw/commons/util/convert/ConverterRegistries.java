3392caaeafb274f0403850df4b5d6c44354f4068
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-15 15:45:51 +0100

    Improved ConverterRegistry

--- aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/convert/ConverterRegistries.java
+++ aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/convert/ConverterRegistries.java
@@ -1,19 +1,77 @@
 package org.aksw.commons.util.convert;
 
+import java.math.BigDecimal;
+
 import com.google.common.primitives.Primitives;
 
 public class ConverterRegistries {
-	/** If the initial lookup fails and any argument is a primitive type then retry with boxed types */
-	public static ConvertFunctionRaw getConverterBoxed(ConverterRegistry registry, Class<?> from, Class<?> to) {
-		ConvertFunctionRaw result = registry.getConverter(from, to);
+    private static ConverterRegistry DFT_REGISTRY = null;
 
-		Class<?> wrappedFrom = Primitives.wrap(from);
-		Class<?> wrappedTo = Primitives.wrap(to);
+    public static ConverterRegistry getDefaultConverterRegistry() {
+        if (DFT_REGISTRY == null) {
+            synchronized (ConverterRegistry.class) {
+                if (DFT_REGISTRY == null) {
+                    DFT_REGISTRY = new ConverterRegistryImpl();
+                    addDefaultConversions(DFT_REGISTRY);
+                }
+            }
+        }
+        return DFT_REGISTRY;
+    }
 
-		if (wrappedFrom != from || wrappedTo != to) {
-			result = registry.getConverter(wrappedFrom, wrappedTo);
-		}
+    /** If the initial lookup fails and any argument is a primitive type then retry with boxed types */
+    public static ConvertFunctionRaw getConverterBoxed(ConverterRegistry registry, Class<?> from, Class<?> to) {
+        ConvertFunctionRaw result = registry.getConverter(from, to);
 
-		return result;
-	}
+        Class<?> wrappedFrom = Primitives.wrap(from);
+        Class<?> wrappedTo = Primitives.wrap(to);
+
+        if (wrappedFrom != from || wrappedTo != to) {
+            result = registry.getConverter(wrappedFrom, wrappedTo);
+        }
+
+        return result;
+    }
+
+    public static Object convert(ConverterRegistry converterRegistry, Object input, Class<?> target) {
+        Object result;
+
+        if (input == null) {
+            result = null;
+        } else {
+            Class<?> inputClass = input.getClass();
+
+            // This method takes into account widenings of primitive classes
+            if (org.apache.commons.lang3.ClassUtils.isAssignable(inputClass, target)) {
+                result = input;
+            } else {
+                // ConvertFunctionRaw converter = converterRegistry.getConverter(inputClass, target);
+                ConvertFunctionRaw converter = ConverterRegistries.getConverterBoxed(converterRegistry, inputClass, target);
+
+                if (converter == null) {
+                    throw new RuntimeException(String.format("No converter registered from %1$s to %2$s", inputClass, target));
+                }
+
+                result = converter.convertRaw(input);
+            }
+        }
+
+        return result;
+    }
+
+    public static void addDefaultConversions(ConverterRegistry registry) {
+        registry
+            .register(BigDecimal.class, Long.class,
+                    BigDecimal::longValueExact, BigDecimal::new)
+            .register(BigDecimal.class, Integer.class,
+                    BigDecimal::intValueExact, BigDecimal::new)
+            .register(BigDecimal.class, Short.class,
+                    BigDecimal::shortValueExact, BigDecimal::new)
+            .register(BigDecimal.class, Byte.class,
+                    BigDecimal::byteValueExact, BigDecimal::new)
+            .register(BigDecimal.class, Double.class,
+                    BigDecimal::doubleValue, BigDecimal::new)
+            .register(BigDecimal.class, Float.class,
+                    BigDecimal::floatValue, BigDecimal::new);
+    }
 }
--- aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/convert/ConverterRegistry.java
+++ aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/convert/ConverterRegistry.java
@@ -35,5 +35,4 @@ public interface ConverterRegistry {
 
         return this;
     }
-
 }
