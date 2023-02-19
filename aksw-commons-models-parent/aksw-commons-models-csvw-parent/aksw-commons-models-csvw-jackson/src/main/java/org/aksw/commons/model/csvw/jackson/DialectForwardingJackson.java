59b681e44ab440383c8f0ea63ce53265d2fed71e
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-19 17:01:04 +0100

    Added jackson bindings for CSVW; also moved 2 generic util classes from sansa-stack to here in order to facilitate reuse in jenax

---
+++ aksw-commons-models-parent/aksw-commons-models-csvw-parent/aksw-commons-models-csvw-jackson/src/main/java/org/aksw/commons/model/csvw/jackson/DialectForwardingJackson.java
@@ -0,0 +1,19 @@
+package org.aksw.commons.model.csvw.jackson;
+
+import org.aksw.commons.model.csvw.domain.api.Dialect;
+import org.aksw.commons.model.csvw.domain.impl.DialectForwardingBase;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/** Annotated forwarding dialect implementation that tells
+ * Jackson to only rely on this class' bean properties
+ * (excluding getDelegate). */
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE)
+@JsonIgnoreProperties({"delegate", "lineTerminatorList"})
+public class DialectForwardingJackson<T extends Dialect>
+        extends DialectForwardingBase<T> {
+    public DialectForwardingJackson(T delegate) {
+        super(delegate);
+    }
+}
