40da8bf3cacdede0398279e795435c83721ee10c
Claus Stadler <RavenArkadon@googlemail.com>
2023-02-21 21:10:44 +0100

    Moved some ObjectUtils for casting from jenax to here

--- aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/obj/ObjectUtils.java
+++ aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/obj/ObjectUtils.java
@@ -2,7 +2,10 @@ package org.aksw.commons.util.obj;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Supplier;
+
+import org.apache.commons.lang3.ClassUtils;
 
 public class ObjectUtils {
 
@@ -20,6 +23,29 @@ public class ObjectUtils {
     public static String toStringWithIdentityHashCode(Object obj, String nullDefault) {
         return obj != null ? obj.getClass().getName() + "@" + System.identityHashCode(obj) : nullDefault;
     }
+
+    public static boolean canCastAs(Class<?> clazz, Object o) {
+        boolean result = o == null ? true : ClassUtils.isAssignable(clazz, o.getClass());
+        return result;
+    }
+
+    public static <T> Optional<T> tryCastAs(Class<T> clazz, Object o) {
+        boolean canCastAs = canCastAs(clazz, o);
+        Optional<T> result = canCastAs ? Optional.of(castAs(clazz, o)) : Optional.empty();
+        return result;
+    }
+
+    public static <T> T castAs(Class<T> clazz, Object o) {
+        @SuppressWarnings("unchecked")
+        T result = (T)o;
+        return result;
+    }
+
+    public static <T> T castAsOrNull(Class<T> clazz, Object o) {
+        T result = canCastAs(clazz, o) ? castAs(clazz, o) : null;
+        return result;
+    }
+
 
     /**
      * Supplier-based coalesce function as described in
