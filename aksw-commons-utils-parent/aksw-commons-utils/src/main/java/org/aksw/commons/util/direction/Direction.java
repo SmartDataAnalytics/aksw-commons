bfce2b1ccb19588d09af4e2b386a3b1663fc795a
Claus Stadler <RavenArkadon@googlemail.com>
2023-06-23 22:46:17 +0200

    Added common Direction util class

---
+++ aksw-commons-utils-parent/aksw-commons-utils/src/main/java/org/aksw/commons/util/direction/Direction.java
@@ -0,0 +1,28 @@
+package org.aksw.commons.util.direction;
+
+/**
+ * An enumeration for representing forward and backward directions.
+ * Booleans quickly become confusing.
+ */
+public enum Direction {
+    FORWARD(true), BACKWARD(false);
+
+    boolean isForward;
+
+    Direction(boolean isForward) {
+        this.isForward = isForward;
+    }
+
+    public boolean isForward() {
+        return isForward;
+    }
+
+    public boolean isBackward() {
+        return !isForward;
+    }
+
+    public static Direction ofFwd(boolean isFwd) {
+        Direction result = isFwd ? FORWARD : BACKWARD;
+        return result;
+    }
+}
